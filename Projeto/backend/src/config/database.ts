cb84f52dfe002bc76675c490bf34fb856c14ff6c
kakashinho <joaovitor.s.c.siqueira@gmail.com>
2025-09-11 00:25:34 -0300

    feat: inicialização de conexão com o banco de dados

---
+++ Projeto/backend/src/config/database.ts
@@ -0,0 +1,27 @@
+import 'reflect-metadata'
+import { DataSource } from 'typeorm'
+import { User } from '../modules/models/usuario'
+//dotenv permite ler variáveis do .env
+import * as dotenv from "dotenv"
+dotenv.config()
+
+export const AppDataSource = new DataSource({
+    type: 'mysql',
+    host: process.env.DB_HOST!,
+    port: Number(process.env.DB_PORT!),
+    username: process.env.DB_USERNAME!,
+    password: process.env.DB_PASSWORD!,
+    database: process.env.DB_NAME!,
+    synchronize: true,
+    logging: true,
+    entities: [User],
+    subscribers: [],
+    migrations: [__dirname + './migration/*.js'],
+})
+
+AppDataSource.initialize()
+    .then(()  => {
+        console.log('Conexão Realizada com Sucesso!')
+    }).catch((error) => {
+        console.log('Erro na conexão com o banco de dados:', error)
+    })
