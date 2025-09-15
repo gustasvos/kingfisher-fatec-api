877cc92a0a507baa6ac868146e6a02e81a1fb939
kakashinho <joaovitor.s.c.siqueira@gmail.com>
2025-09-15 20:25:31 -0300

    feat: inserindo validações de CPF, EMAIL, Telefone, SENHA, e a variavel ambiente saltRounds

--- Projeto/backend/src/modules/controllers/UsersControllers.ts
+++ Projeto/backend/src/modules/controllers/UsersControllers.ts
@@ -4,20 +4,67 @@ import { User } from '../models/usuario'
 import { Not } from 'typeorm'
 import { console } from 'inspector'
 import bcrypt from 'bcrypt'
+import { validarCPF } from '../../utils/validarCPF'
+import { validarEmail } from '../../utils/validarEmail'
+import { validarSenha } from '../../utils/validarSenha'
+import { validarTelefone } from '../../utils/validarTelefone'
+//dotenv permite ler variáveis do .env
+import * as dotenv from "dotenv"
+dotenv.config()
 
 const router = express.Router()
-
-router.post('/user',async (req: Request, res: Response) => {
+/*
+    Json do Front-end
+    {
+        "nome":     "string",
+        "cpf":      "string",
+        "email":    "string",
+        "telefone": "string",
+        "cargo":    "string",
+        "senha":    "string",
+        "data_de_contratacao": "2024-06-12",
+    }
+ */
+router.post('/usuario/create',async (req: Request, res: Response) => {
     try{
         var data = req.body
+
+        if(!validarCPF(data.cpf)){
+            res.status(400).json({
+                mesage:'CPF inválido!'
+            })
+            return
+        }
+
+        if(!validarEmail(data.email)){
+            res.status(400).json({
+                mesage:'Email inválido!'
+            })
+            return
+        }
+
+        if(!validarTelefone(data.telefone)){
+            res.status(400).json({
+                mesage:'Telefone inválido!'
+            })
+            return
+        }
+
+        if (!validarSenha(data.senha)) {
+            res.status(400).json({
+                message: 'Senha inválida! A senha deve ter pelo menos 8 caracteres, incluindo letras maiúsculas, minúsculas, números e caracteres especiais.'
+            })
+            return
+        }
 
         const userRepository = AppDataSource.getRepository(User)
 
         const existingUser = await userRepository.findOne({
-            where: {
-                cpf: data.cpf,
-                email: data.email
-            }
+            //condição OR no TypeORM
+            where: [
+                {cpf: data.cpf},
+                {email: data.email}
+            ]            
         })
         if(existingUser){
             res.status(400).json({
@@ -25,7 +72,7 @@ router.post('/user',async (req: Request, res: Response) => {
             })
         return
         }
-        const saltRounds = 10
+        const saltRounds = Number(process.env.SALT_ROUNDS)
 
         const senha = data.senha
         const hash = await bcrypt.hash(senha, saltRounds)
