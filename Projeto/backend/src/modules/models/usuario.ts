6cb981678fb5f076d1914f0ba9630cf500a65897
kakashinho <joaovitor.s.c.siqueira@gmail.com>
2025-09-20 11:40:24 -0300

    feat: inserindo os novos campos do banco de dados para cadastrar um usuario

--- Projeto/backend/src/modules/models/usuario.ts
+++ Projeto/backend/src/modules/models/usuario.ts
@@ -1,4 +1,6 @@
 import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm'
+import { IsDate } from 'class-validator'
+import { Sexo, EstadoCivil, TipoEndereco, TipoAcesso } from '../../utils/enums/usuarioEnums'
 
 @Entity('usuario')
 export class User {
@@ -8,14 +10,105 @@ export class User {
     @Column({type: 'varchar', length: 255})
     nome: string
 
+    @Column({type: 'varchar', length: 255})
+    nome_fantasia: string
+
     @Column({type: 'varchar', length: 11, unique: true})
     cpf: string
+
+    @Column({type: 'enum', enum: Sexo})
+    sexo: Sexo
+
+    @Column({type: 'enum', enum: EstadoCivil})
+    estado_civil: EstadoCivil
+
+    @IsDate()
+    @Column({type: 'date'})
+    data_nascimento: Date
+
+    @Column({ type: 'varchar', length: 100})
+    cidade_nascimento: string
+
+    @Column({ type: 'varchar', length: 10, unique: true})
+    rg: string
+
+    @Column({ type: 'varchar', length: 100})
+    cidade_expedicao_rg: string
+
+    @Column({ type: 'varchar', length: 10})
+    orgao_expedidor: string
+
+    @IsDate()
+    @Column({ type: 'date', nullable: true })
+    data_emissao_rg: Date
+
+
+    @Column({type: 'varchar', length: 255})
+    documento_exterior: string
+
+    @Column({type: 'varchar', length: 14})
+    inscricao_estadual: string
+
+    @Column({type: 'varchar', length: 11})
+    pis_pasep: string
+
+    @Column({ type: 'varchar', length: 30, unique: true})
+    rntrc: string
+
+    @IsDate()
+    @Column({type: 'date'})
+    validade_rntrc: Date
 
     @Column({ type: 'varchar', length: 255, unique: true})
     email: string
 
-    @Column({ type: 'varchar', length: 15})
+    @Column({ type: 'varchar', length: 20})
     telefone: string
+
+    @Column({ type: 'varchar', length: 20})
+    celular: string
+    
+    @Column({ type: 'varchar', length: 20})
+    operadora: string
+
+    @Column({ type: 'varchar', length: 50})
+    codigo: string
+
+    @Column({ type: 'varchar', length: 255})
+    nome_pai: string
+
+    @Column({ type: 'varchar', length: 255})
+    nome_mae: string
+
+    @Column({ type: 'boolean', default: true})
+    ativo: boolean
+
+    @Column({ type: 'varchar', length: 8})
+    cep: string
+
+    @Column({ type: 'varchar', length: 100})
+    bairro: string
+
+    @Column({ type: 'varchar', length: 255})
+    logradouro: string
+
+    @Column({ type: 'varchar', length: 10})
+    numero: string
+
+    @Column({ type: 'varchar', length: 255, nullable: true})
+    complemento: string
+
+    @Column({ type: 'varchar', length: 100})
+    cidade: string
+
+    @Column({ type: 'enum', enum: TipoEndereco})
+    tipo_endereco: TipoEndereco
+
+    @Column({ type: 'decimal', precision: 10, scale: 7, nullable: true})
+    latitude: number
+    
+    @Column({ type: 'decimal', precision: 10, scale: 7, nullable: true })
+    longitude: number
 
     @Column({ type: 'varchar', length: 100})
     cargo: string
@@ -23,42 +116,10 @@ export class User {
     @Column({ type: 'varchar', length: 255})
     senha: string
 
+    @IsDate()
     @Column({type: 'date'})
     data_contratacao: Date
 
-    @Column({ type: 'varchar', length: 50, default: 'usuario' })
-    role: string
+    @Column({ type: 'enum', enum: TipoAcesso, default: 'usuario' })
+    role: TipoAcesso
 }
-// cpf ok
-// nome completo ok
-// nome fantasia not ok
-// sexo
-// estado civil
-// data nascimento
-// cidade de nascimento
-// RG
-// Cidade exoedição RG
-// Orgão expedidor
-// Data emissão RG
-// Documento Exterior
-// inscrição estadual
-// PIS/PASEP
-// RNTRC
-// validade RNTRC
-// telefone ok
-// celular
-// Operadora
-// email ok
-// código
-// Nome do pai
-// Nome da mãe
-// Ativo
-// CEP
-// Bairro
-// Logradouro
-// Número
-// Complemento
-// Cidade
-// tipo de endereço
-// latitude
-// longitude
