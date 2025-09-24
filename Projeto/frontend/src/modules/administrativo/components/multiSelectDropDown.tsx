5a55f7ecbb4a92100f4f12c37b16357ab14bcb52
aegsjc@gmail.com <aegsjc@gmail.com>
2025-09-24 17:23:01 -0300

    style:ajuste do dropdown (US02)

--- Projeto/frontend/src/modules/administrativo/components/multiSelectDropDown.tsx
+++ Projeto/frontend/src/modules/administrativo/components/multiSelectDropDown.tsx
@@ -4,60 +4,19 @@ import axios from "axios";
 
 const MultiSelectDropdown = () => {
     const [options, setOptions] = useState([]);
-    const [selectedOptions, setSelectedOptions] = useState([]);
+    const [selectedOptions, setSelectedOptions] = useState([])
 
     useEffect(() => {
-        axios.get("http://localhost:8080/usuario/list")
+        axios.get("http://localhost:8080/usuarios")
             .then((response) => {
                 const usuarios = response.data
                 console.log(usuarios)
                 const formattedOptions = usuarios.map((item: any) => ({
-                    id: item.id,
-                    nome: item.nome
+                    value: item.id,
+                    label: item.nome
                 }));
-                const a: any = [
-                    {
-                        "id": 1,
-                        "nome": "Thiago Lima",
-                        "nome_fantasia": "Thiago Lima LTDA",
-                        "cpf": "85107366007",
-                        "sexo": "M",
-                        "estado_civil": "solteiro",
-                        "data_nascimento": "1985-04-20",
-                        "cidade_nascimento": "Rio de Janeiro",
-                        "rg": "987656521",
-                        "cidade_expedicao_rg": "Rio de Janeiro",
-                        "orgao_expedidor": "SSP",
-                        "data_emissao_rg": "2005-07-15",
-                        "documento_exterior": "",
-                        "inscricao_estadual": "9876543210",
-                        "pis_pasep": "98765432101",
-                        "rntrc": "987654321012345",
-                        "validade_rntrc": "2027-05-30",
-                        "email": "thiago.lima@email.com",
-                        "telefone": "11999988877",
-                        "celular": "11999988877",
-                        "operadora": "Claro",
-                        "codigo": "THI123",
-                        "nome_pai": "José Lima",
-                        "nome_mae": "Ana Lima",
-                        "ativo": true,
-                        "cep": "22040002",
-                        "bairro": "Copacabana",
-                        "logradouro": "Avenida Atlântica",
-                        "numero": "456",
-                        "complemento": "Sala 10",
-                        "cidade": "Rio de Janeiro",
-                        "tipo_endereco": "residencial",
-                        "latitude": "-22.9719640",
-                        "longitude": "-43.1825430",
-                        "cargo": "Product Owner",
-                        "senha": "$2b$12$ZCbdvFJmICyxvZcvz8ZQCOevHXpFtCpLqUtzj.8fTYpkcFrx46HxK",
-                        "data_contratacao": "2025-09-01",
-                        "role": "admin"
-                    }
-                ]
-                setOptions(a);
+
+                setOptions(formattedOptions);
             })
             .catch((error) => console.error("Erro ao buscar dados:", error));
     }, []);
@@ -66,9 +25,9 @@ const MultiSelectDropdown = () => {
         setSelectedOptions(selected);
     };
 
-    return <div className="w-[300px] mx-auto">
-        <label className="block text-sm font-medium text-gray-700 mb-2">
-            Select Items
+    return <div className="w-[300px] h-[200px] space-y-4">
+        <label className="font-sans text-[#053657] font-medium">
+            Convidados
         </label>
         <Select
             isMulti
@@ -77,6 +36,18 @@ const MultiSelectDropdown = () => {
             onChange={handleChange}
             className="react-select-container"
             classNamePrefix="react-select"
+            menuPortalTarget={document.body}
+            menuPosition="fixed"
+            placeholder="Selecione os colaboradores que serão convidados"
+            required
+            styles={{
+                control: (provided) => ({
+                    ...provided,
+                    minHeight: "30px",
+                    maxHeight: "250px",
+                    overflowY: "auto"
+                })
+            }}
         />
     </div>
 }
--- Projeto/frontend/src/modules/administrativo/components/novoEvento.tsx
+++ Projeto/frontend/src/modules/administrativo/components/novoEvento.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import Botao from "../../../shared/components/botao";
 import Container from "../../../shared/components/container";
 import MultiSelectDropdown from "./multiSelectDropDown";
@@ -5,38 +6,62 @@ import MultiSelectDropdown from "./multiSelectDropDown";
 
 export default function NovoEvento() {
 
+    const [mensagem, setMensagem] = useState(false)
+    const [titulo, setTiulo] = useState('')
+    const [descricao, setDescricao] = useState('')
+    const [data, setData] = useState('')
+    const [localizacao, setLocalizacao] = useState('')
+    const [convidados,setConvidados] = useState([])
+
+    const formEnviado = (e: React.FormEvent) => {
+        e.preventDefault()
+        setMensagem(true)
+    }
 
     return (
         <Container>
-            <form action="" className="grid grid-cols-[50%,50%] grid-rows-4">
+            <form action="" onSubmit={formEnviado} className="grid grid-cols-[50%,50%]">
                 <section className="flex flex-col items-center pt-[25px] space-y-4">
                     <p className="font-sans text-[25px] text-[#053657] font-semibold">Criação de Novo Evento</p>
                     <section>
                         <p className="font-sans text-[#053657] font-medium">Título</p>
-                        <input type="text" placeholder="Digite o título do evento" className="w-[300px] h-[30px] p-3 rounded-[8px] outline-[#053657]" />
+                        <input type="text" placeholder="Digite o título do evento" value={titulo} onChange={(e) => setTiulo(e.target.value)} required className="w-[300px] h-[30px] p-3 rounded-[8px] outline-[#053657]" />
                     </section>
                     <section>
                         <p className="font-sans text-[#053657] font-medium">Descrição</p>
-                        <textarea placeholder="Digite a descrição do evento" className="w-[300px] h-[150px] p-3 rounded-[8px] outline-[#053657]" name="" id=""></textarea>
+                        <textarea placeholder="Digite a descrição do evento" value={descricao} onChange={(e) => setDescricao(e.target.value)} required className="w-[300px] h-[150px] p-3 rounded-[8px] outline-[#053657]" name="" id=""></textarea>
                     </section>
                     <section>
                         <p className="font-sans text-[#053657] font-medium">Data</p>
-                        <input type="date" className="w-[300px] h-[30px] p-3 rounded-[8px] outline-[#053657]" />
+                        <input type="date" value={data} onChange={(e) => setData(e.target.value)} required className="w-[300px] h-[30px] p-3 rounded-[8px] outline-[#053657]" />
                     </section>
                     <section>
                         <p className="font-sans text-[#053657] font-medium">Local</p>
-                        <input type="text" placeholder="Digite o local do evento" className="w-[300px] h-[30px] p-3 rounded-[8px] outline-[#053657]" />
+                        <input type="text" placeholder="Digite o local do evento" value={localizacao} onChange={(e) => setLocalizacao(e.target.value)} required className="w-[300px] h-[30px] p-3 rounded-[8px] outline-[#053657]" />
                     </section>
                 </section>
-                <section className="flex flex-col items-center justify-between pt-[25px] ">
-
-                    <MultiSelectDropdown/>
-
-                    <Botao>
-                        Salvar
-                    </Botao>
+                <section className="flex flex-col items-center justify-between pt-[38px] ">
+                    <MultiSelectDropdown />
+                    <input type="submit" value={'ENVIAR'} className="bg-[#015084] w-[250px] h-[45px] rounded-[8px] text-white text-[15px] font-semibold cursor-pointer font-sans" />
                 </section>
+
             </form>
+            {mensagem && (
+                <section className="fixed inset-0 bg-black bg-opacity-40 flex justify-center items-center">
+                    <section className="bg-white rounded-[15px] p-8 drop-shadow-lg flex flex-col justify-center items-center">
+                        <p className="text-black">Resposta enviada com sucesso</p>
+                        <button onClick={() => {
+                            setMensagem(false)
+                            setTiulo('')
+                            setDescricao('')
+                            setData('')
+                            setLocalizacao('')
+                        }} className="mt-4 bg-[#015084]  text-white px-4 py-2 rounded">
+                            Fechar
+                        </button>
+                    </section>
+                </section>
+            )}
         </Container>
     )
 }
