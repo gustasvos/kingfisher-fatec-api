8cf7e306ef2908ebf77c40372001c7f424e375f2
kakashinho <joaovitor.s.c.siqueira@gmail.com>
2025-09-26 19:41:30 -0300

    feat: configurando o cors

--- Projeto/backend/src/index.ts
+++ Projeto/backend/src/index.ts
@@ -1,11 +1,15 @@
 //Ponto de entrada do backend, (como o main.tsx do front)
 import express, {Request, Response} from 'express'
-import cors from 'cors'
 import usuariosRoutes from './modules/routers/usuariosRoutes';
+const cors = require('cors');
 
 const app = express()
 app.use(express.json())
-app.use(cors())
+app.use(cors({
+  origin: 'http://localhost:3000', // ou '*' para liberar geral (não recomendado em produção)
+  methods: ['GET','POST','PUT','DELETE'],
+  credentials: true // se você usa cookies, tokens etc
+}));
 
 app.use('/',usuariosRoutes)
 app.get('/', (req:Request, res:Response) =>{
