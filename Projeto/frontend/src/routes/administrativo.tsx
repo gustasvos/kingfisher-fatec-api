e1c63ec6db525d775b073a3c1a35f2188a7c4558
kakashinho <joaovitor.s.c.siqueira@gmail.com>
2025-09-26 19:46:29 -0300

    feat: inserindo a rota de cadastrar usuário

--- Projeto/frontend/src/routes/administrativo.tsx
+++ Projeto/frontend/src/routes/administrativo.tsx
@@ -1,5 +1,6 @@
 import { Route } from "react-router-dom"
 import LoginPage from "../modules/administrativo/pages/login-page"
+import Cadastro from '../modules/administrativo/pages/PaginaCadastro';
 import EventoDetalhePage from "../modules/administrativo/pages/eventoDetalhe-page"
 
 
@@ -7,6 +8,7 @@ export default function RotasADM(){
     return (
         <>
             <Route path="/" element={<LoginPage/>}/>
+            <Route path="/cadastrar" element={<Cadastro />} />
             <Route path="/eventoDetalhe" element={<EventoDetalhePage/>}/>
         </>
     )
