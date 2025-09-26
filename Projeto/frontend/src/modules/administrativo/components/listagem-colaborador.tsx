4ac7efeae32c992f7691b809feabfec54d454b8a
maria <mariaf.laboissiere@gmail.com>
2025-09-26 10:49:52 -0300

    feat: criação da tela listagem de colaboradores

---
+++ Projeto/frontend/src/modules/administrativo/components/listagem-colaborador.tsx
@@ -0,0 +1,14 @@
+import Navbar from "../../../shared/components/navbar"
+
+export default function ListagemColaborador(){
+    return (
+        <section className="bg-slate-700">
+            <section>
+                <Navbar/>
+            </section>
+            <section >
+                <p>teste</p>
+            </section>
+        </section>
+    )
+}
--- Projeto/frontend/src/routes/administrativo.tsx
+++ Projeto/frontend/src/routes/administrativo.tsx
@@ -1,6 +1,7 @@
 import { Route } from "react-router-dom"
 import LoginPage from "../modules/administrativo/pages/login-page"
 import EventoDetalhePage from "../modules/administrativo/pages/eventoDetalhe-page"
+import PageListagemColaborador from "../modules/administrativo/pages/listagem-colaborador-page"
 
 
 export default function RotasADM(){
@@ -8,6 +9,7 @@ export default function RotasADM(){
         <>
             <Route path="/" element={<LoginPage/>}/>
             <Route path="/eventoDetalhe" element={<EventoDetalhePage/>}/>
+            <Route path="/listagemColaborador" element={<PageListagemColaborador/>}/>
         </>
     )
 }
