5f55d51c246d380ad0988196c85cca1bc8c9f8f6
kakashinho <joaovitor.s.c.siqueira@gmail.com>
2025-09-26 19:43:39 -0300

    refactor: converti os arquivos de react com typescript

--- Projeto/frontend/src/modules/administrativo/components/InputField.jsx
+++
@@ -1,8 +0,0 @@
-export default function InputField({ label, type, placeholder }) {
-  return (
-    <div className="input">
-      <label className="label">{label}</label>
-      <input type={type} placeholder={placeholder} className="input-style" />
-    </div>
-  );
-}
