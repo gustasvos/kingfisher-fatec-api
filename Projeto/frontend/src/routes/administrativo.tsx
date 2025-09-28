07177c41667245e2c4bd09ce5303f675526ed1fc
kakashinho <joaovitor.s.c.siqueira@gmail.com>
2025-09-28 03:37:10 -0300

    refactor: atualizando as rotas

--- Projeto/frontend/src/routes/administrativo.tsx
+++ Projeto/frontend/src/routes/administrativo.tsx
@@ -2,8 +2,13 @@ import { Route } from "react-router-dom"
 import LoginPage from "../modules/administrativo/pages/login-page"
 import Cadastro from '../modules/administrativo/pages/PaginaCadastro';
 import EventoDetalhePage from "../modules/administrativo/pages/eventoDetalhe-page"
-import PageListagemColaborador from "../modules/administrativo/pages/listagem-colaborador-page"
+import ListagemColaborador from "../modules/administrativo/pages/listagem-colaborador-page"
 import NovoEvento from "../modules/administrativo/components/novoEvento";
+import PaginaEventos from "../modules/administrativo/components/pagina-eventos";
+import HomePage from "../modules/administrativo/pages/home-Page";
+
+
+
 
 
 export default function RotasADM(){
@@ -11,9 +16,11 @@ export default function RotasADM(){
         <>
             <Route path="/" element={<LoginPage/>}/>
             <Route path="/cadastrar" element={<Cadastro />} />
-            <Route path="/eventoDetalhe" element={<EventoDetalhePage/>}/>
-            <Route path="/listagemColaborador" element={<PageListagemColaborador/>}/>
-            <Route path="/novoEvento" element={<NovoEvento/>}/>
+            <Route path="/evento-convite" element={<EventoDetalhePage/>}/>
+            <Route path="/colaboradores" element={<ListagemColaborador/>}/>
+            <Route path="/eventos" element={<PaginaEventos/>}/>
+            <Route path="/novo-evento" element={<NovoEvento/>}/>
+            <Route path="/home" element={<HomePage/>} />
         </>
     )
 }
