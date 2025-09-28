586f2afc8bd6fbb997b825c09ed90358189f65e0
kakashinho <joaovitor.s.c.siqueira@gmail.com>
2025-09-28 03:30:20 -0300

    feat: inserindo nova rota

--- Projeto/backend/src/modules/routers/usuariosRoutes.ts
+++ Projeto/backend/src/modules/routers/usuariosRoutes.ts
@@ -1,16 +1,18 @@
 import express from 'express'
-import { createUsuario, listUsuario,listUsuarioById, updateUsuario, deleteUsuario, loginUsuario, logoutUsuario } from '../controllers/UsersControllers'
+import { createUsuario, listUsuario,listUsuarioById, updateUsuario, deleteUsuario, loginUsuario, logoutUsuario, checkIfUsersExist } from '../controllers/UsersControllers'
 import { autenticarUsuario } from '../../middlewares/auth.middleware'
 import { autorizarUsuario } from '../../middlewares/autorizar.middleware'
 
 const router = express.Router()
 
+//autenticarUsuario, autorizarUsuario(['admin','usuario']),
 router.post('/usuario/create', createUsuario)
 router.post('/login', loginUsuario)
 router.post('/logout', autenticarUsuario, logoutUsuario)
 router.get('/usuario/list', listUsuario);
-router.get('/usuario/:id', autenticarUsuario, autorizarUsuario(['admin','usuario']), listUsuarioById);
-router.put('/usuario/:id', autenticarUsuario, autorizarUsuario(['admin','usuario']), updateUsuario);
-router.delete('/usuario/:id', autenticarUsuario, autorizarUsuario(['admin','usuario']), deleteUsuario);
+router.get('/usuario/:id', autenticarUsuario, listUsuarioById);
+router.put('/usuario/:id', updateUsuario);
+router.delete('/usuario/:id', deleteUsuario);
+router.get("/users/exists", checkIfUsersExist);
 
 export default router
