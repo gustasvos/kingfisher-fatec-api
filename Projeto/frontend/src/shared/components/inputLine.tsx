63b098208e10ae69118d87e44cb03aadd229d7f9
Laís Zanardi Inocêncio <89917698+lais-zanardi@users.noreply.github.com>
2025-10-10 15:49:43 -0300

    <feat> (US-05) adiciona propriedade placeholder ao componente input line

--- Projeto/frontend/src/shared/components/inputLine.tsx
+++ Projeto/frontend/src/shared/components/inputLine.tsx
@@ -9,10 +9,11 @@ type PropriedadeInput = {
     required?: boolean
     value?: string,
     checked?: boolean,
+    placeholder?: string,
     onchange?:(e: React.ChangeEvent<HTMLInputElement>) =>void
 }
 
-export default function InputLine({ classNameInput, classNameLabel, id, htmlfor, children, type, name, value, required, checked,onchange }: PropriedadeInput) {
+export default function InputLine({ classNameInput, classNameLabel, id, htmlfor, children, type, name, value, required, checked,onchange, placeholder }: PropriedadeInput) {
     const tipoRadio = type === 'radio'
     const tipoFile = type === 'file'
     if (tipoRadio) {
@@ -36,7 +37,7 @@ export default function InputLine({ classNameInput, classNameLabel, id, htmlfor,
     }
     return (
         <section className="relative">
-            <input type={type} name={name} id={id} required={required} onChange={onchange} className={`w-[300px] block rounded-t-lg px-2.5 pb-2.5 pt-5 text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer ${classNameInput}`} />
+            <input type={type} name={name} placeholder={placeholder} id={id} required={required} onChange={onchange} className={`w-[300px] block rounded-t-lg px-2.5 pb-2.5 pt-5 text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer ${classNameInput}`} />
             <label htmlFor={htmlfor} className={`absolute text-sm text-gray-500 duration-300 transform -translate-y-4 scale-75 top-4 z-10 origin-[0] start-2.5 peer-focus:text-blue-600 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-4 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto ${classNameLabel}`}>
                 {children}
             </label>
