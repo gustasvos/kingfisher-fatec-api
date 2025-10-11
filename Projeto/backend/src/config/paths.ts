16296bd82401b45cb7d296334ccb0fb0a0fb289d
kakashinho <joaovitor.s.c.siqueira@gmail.com>
2025-10-11 10:21:40 -0300

    <feat>(US05): Incrementação de paths para diretórios de armazenamento de uploads como arquivos de imagem e de submit como csv, com a finalidade de simplificar o acesso a esses diretórios

---
+++ Projeto/backend/src/config/paths.ts
@@ -0,0 +1,5 @@
+import path from 'path';
+
+export const uploadsDir = path.join(__dirname, '../../uploads');
+export const dataDir = path.join(__dirname, '../../data');
+export const csvPath = path.join(dataDir, 'dados.csv');
