4b085c86e477bef685629cc5ece44d71a1fe3827
kakashinho <joaovitor.s.c.siqueira@gmail.com>
2025-10-11 10:14:18 -0300

    <feat>(US05): Implementação da rota de formulário, middlware estático para uploads e inicialização das pastas 'data' e 'upload' para as funcionalidades de formulários

--- Projeto/backend/src/index.ts
+++ Projeto/backend/src/index.ts
@@ -2,18 +2,27 @@
 import express, {Request, Response} from 'express'
 import usuariosRoutes from './modules/routers/usuariosRoutes';
 import EventoRoutes from './modules/routers/EventoRoutes';
+import formRoutes from './modules/routers/formRoutes'
+import path from 'path';
+import { uploadsDir,dataDir } from './config/paths'
+import { initFolders } from './utils/initFolders';
+initFolders();
 const cors = require('cors');
 
 const app = express()
 app.use(express.json())
+app.use(express.urlencoded({ extended: true }));
 app.use(cors({
-  origin: 'http://localhost:3000', // ou '*' para liberar geral (não recomendado em produção)
+  origin: 'http://localhost:3000',
   methods: ['GET','POST','PUT','DELETE'],
   credentials: true // se você usa cookies, tokens etc
 }));
 
 app.use('/',EventoRoutes)
 app.use('/',usuariosRoutes)
+app.use("/uploads", express.static(uploadsDir));
+app.use("/data", express.static(dataDir));
+app.use("/", formRoutes);
 app.get('/', (req:Request, res:Response) =>{
     res.send('Bem Vindo')
 })
