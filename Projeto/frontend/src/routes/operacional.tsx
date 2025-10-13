ce76d935d51de35273815d0f19407cf65935a193
ana <aegsjc@gmail.com>
2025-10-13 10:48:56 -0300

    <feat>(US-05): adiciona rota para a tela de formulário de fechamento

--- Projeto/frontend/src/routes/operacional.tsx
+++ Projeto/frontend/src/routes/operacional.tsx
@@ -2,6 +2,7 @@ import { Route } from "react-router-dom"
 import CheckVeiculosPage from "../modules/operacional/pages/check-veiculos-page"
 import CheckDiarioPage from "../modules/operacional/pages/check-diario-page"
 import FormAberturaPage from "../modules/operacional/pages/form-abertura-page"
+import FormFechamentoPage from "../modules/operacional/pages/form-fechamento-page"
 
 export default function RotasOp(){
     return(
@@ -9,6 +10,7 @@ export default function RotasOp(){
             <Route path="/check-veiculo" element={<CheckVeiculosPage/>}/>
             <Route path="/check-diario" element={<CheckDiarioPage/>}/>
             <Route path="/form-abertura" element={<FormAberturaPage/>}/>
+            <Route path="/form-fechamento" element={<FormFechamentoPage/>}/>
         </>
     )
 }
