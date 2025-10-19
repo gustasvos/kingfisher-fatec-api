30918e05e948978fb69af2aebb3158091f211c9f
Gustavo Rosa <gustavorib.rosa@gmail.com>
2025-10-19 13:04:22 -0300

    <feat> (AN-97): Criação de um componente compartilhado para ser o Modal para telas das páginas.

---
+++ Projeto/frontend/src/shared/components/modal.tsx
@@ -0,0 +1,28 @@
+import React from "react"
+import Botao from "./botao"
+
+type ModalProps = {
+    aberto: boolean
+    onFechar: () => void
+    children: React.ReactNode // conteudo do modal
+}
+
+const Modal: React.FC<ModalProps> = ({ aberto, onFechar, children }) => {
+    if (!aberto) return null
+
+    const handleClickOutside = (e: React.MouseEvent) => {
+        if (e.target === e.currentTarget) {
+            onFechar()
+        }
+    }
+
+    return (
+        <section className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50" onClick={handleClickOutside}>
+            <section className="p-6 w-[30vw] max-w-full max-h-auto overflow-y-auto" onClick={(e) => e.stopPropagation()}>
+                {children}
+            </section>
+        </section>
+    );
+};
+
+export default Modal
