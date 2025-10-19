afe8382843fe967a4c2caae4c8e4bf63f0fda7a6
Gustavo Rosa <gustavorib.rosa@gmail.com>
2025-10-19 18:11:13 -0300

    <fix>: Ajuste no componente de Loading para visualizar melhor na tela

--- Projeto/frontend/src/shared/components/loading.tsx
+++ Projeto/frontend/src/shared/components/loading.tsx
@@ -2,7 +2,7 @@ import React from "react"
 
 export default function Loading(){
     return (
-        <div className="bg-[#EAF7FF] min-h-screen flex items-center justify-center">
+        <div className="absolute inset-0 flex items-center justify-center bg-white/50 backdrop-blur-sm z-50 ">
             <p className="text-[20px] md:text-[38px] font-sans font-bold italic text-gray-600 pt-5 md:drop-shadow-[10px_8px_3px_rgba(0,0,0,0.3)]">
                 Carregando...</p>
         </div>
