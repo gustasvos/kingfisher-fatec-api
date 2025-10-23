364de4194815567840db06f7a8c7381c5f147d74
Kodatoo <gkodatofaria@gmail.com>
2025-10-23 11:22:59 -0300

    <feat> (AN-64): Adição dos campos necessários para cadastro de clientes, e criação da rota do comercial.

--- Projeto/frontend/src/routes/index.tsx
+++ Projeto/frontend/src/routes/index.tsx
@@ -1,11 +1,14 @@
 import { BrowserRouter, Routes } from "react-router-dom";
 import RotasADM from "./administrativo";
+import RotasComercial from "./comercial";
 
 export default function RotasGerais(){
     return(
         <BrowserRouter>
             <Routes>
                 {RotasADM()}
+                {RotasComercial()}
+
             </Routes>
         </BrowserRouter>
     )
