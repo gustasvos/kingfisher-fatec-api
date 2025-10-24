19595383c4b72af8f180ed0c6165bc6f5495d49b
kakashinho <joaovitor.s.c.siqueira@gmail.com>
2025-10-24 09:04:19 -0300

    <feat>(US05): incrementação da funcionalidade de middleware para só permitir a entrada de admin nas rotas.

--- Projeto/frontend/src/routes/administrativo.tsx
+++ Projeto/frontend/src/routes/administrativo.tsx
@@ -7,18 +7,19 @@ import ListagemColaborador from "../modules/administrativo/components/listagem-c
 import HomePage from "../modules/administrativo/pages/home-Page";
 import LocalDeTrabalho from "../modules/administrativo/components/colaboradorLoca";
 import ListagemEventos from "../modules/administrativo/components/listagem-eventos";
+import PrivateWrapper from "./../shared/components/PrivateWrapper";
 
 export default function RotasADM(){
     return (
         <>
             <Route path="/" element={<LoginPage/>}/>
             <Route path="/cadastrar" element={<Cadastro />} />
-            <Route path="/evento-convite" element={<EventoDetalhe/>}/>
-            <Route path="/colaboradores" element={<ListagemColaborador/>}/>
-            <Route path="/eventos" element={<PaginaEventos/>}/>
-            <Route path="/home" element={<HomePage/>} />
-            <Route path="/eventos-colaborador" element={<ListagemEventos />} />
-            <Route path="/local-trabalho" element={<LocalDeTrabalho />} />
+            <Route path="/evento-convite" element={<PrivateWrapper roles={['admin']}><EventoDetalhe/></PrivateWrapper>}/>
+            <Route path="/colaboradores" element={<PrivateWrapper roles={['admin']}><ListagemColaborador/></PrivateWrapper>}/>
+            <Route path="/eventos" element={<PrivateWrapper roles={['admin']}><PaginaEventos/></PrivateWrapper>}/>
+            <Route path="/home" element={<PrivateWrapper roles={['admin']}><HomePage/></PrivateWrapper>} />
+            <Route path="/eventos-colaborador" element={<PrivateWrapper roles={['admin']}><ListagemEventos /></PrivateWrapper>} />
+            <Route path="/local-trabalho" element={<PrivateWrapper roles={['admin']}><LocalDeTrabalho /></PrivateWrapper>} />
         </>
     )
 }
