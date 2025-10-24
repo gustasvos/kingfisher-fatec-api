f65715ddda867fc1aea17d687942b8681e5de3c6
ana <aegsjc@gmail.com>
2025-10-24 12:31:00 -0300

    <feat>(US-05): inicio da integração da tela inicial da home lado do colaborador, módulo operacional

--- Projeto/frontend/src/modules/operacional/pages/home-op-colab.tsx
+++ Projeto/frontend/src/modules/operacional/pages/home-op-colab.tsx
@@ -1,22 +1,23 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import HighlightCard from '../../../shared/components/highlight-card';
 import { FaRegEdit, FaEye, FaCalendar } from 'react-icons/fa'
 import Navbar from '../../../shared/components/navbar';
 import Header from '../../../shared/components/header';
 import { Link, useNavigate } from 'react-router-dom';
 import { Subtitles } from 'lucide-react';
+import axios from 'axios';
 
 
 /**
- * mock para o atalho de preencher checlist 
+ * mock para o atalho de preencher checlist
 */
 const checkNaoPreechido = 1
 const totalCheck = 2
 
 const mockCheckPreencher = {
-    value: checkNaoPreechido,
-    subtitle: `Responda os checklists restantes ${checkNaoPreechido}/${totalCheck}`,
-    icon: FaRegEdit
+  value: checkNaoPreechido,
+  subtitle: `Responda os checklists restantes ${checkNaoPreechido}/${totalCheck}`,
+  icon: FaRegEdit
 }
 
 /**
@@ -25,9 +26,9 @@ const mockCheckPreencher = {
 const checkPreechido = 1
 
 const mockCheckPreenchidos = {
-    value: checkPreechido,
-    subtitle: `Veja os checklists respondidos ${checkPreechido}/${totalCheck}`,
-    icon: FaRegEdit
+  value: checkPreechido,
+  subtitle: `Veja os checklists respondidos ${checkPreechido}/${totalCheck}`,
+  icon: FaRegEdit
 }
 
 /**
@@ -36,9 +37,9 @@ const mockCheckPreenchidos = {
 const totalEventosMes = 2
 
 const mockEventos = {
-    value: totalEventosMes,
-    subtitle: `verifique seus eventos desse mês`,
-    icon: FaCalendar
+  value: totalEventosMes,
+  subtitle: `verifique seus eventos desse mês`,
+  icon: FaCalendar
 }
 
 /**
@@ -68,25 +69,85 @@ const mockData3 = {
 
 
 const HomeOpColabPage: React.FC = () => {
-//   const [data, setData] = useState<DashboardData | null>(mockData);
 
-const navigate = useNavigate()
+  const userId = localStorage.getItem("userId")
+  const token = localStorage.getItem("token")
 
-const irEventosCOlaborador = () =>{
+  const [eventos, setEventos] = useState<any[]>([])
+  const [statusCounts, setStatusCounts] = useState({ total: 0, aguardando: 0 })
+  const [eventoResp, setEventoResp] = useState<any[]>([])
+  const [eventoRespCount, setEventoRespCount] = useState({ respondido: 0 })
+
+  //   const [data, setData] = useState<DashboardData | null>(mockData);
+
+  const navigate = useNavigate()
+
+  const irEventosColaborador = () => {
     navigate("/eventos-colaborador")
-}
+  }
 
-const irRespostaEventos = () =>{
+  const irRespostaEventos = () => {
     navigate("/resposta-eventos")
-}
+  }
 
-const irEventos = () =>{
+  const irEventos = () => {
     navigate("/eventos")
-}
+  }
 
-const irEventosColaborador = () =>{
-    navigate("/eventos-colaborador")
-}
+  const irCheckColaborador = () => {
+    navigate("/lista-check-colaborador")
+  }
+
+  useEffect(() => {
+    if (!userId || !token) return;
+  
+    const headers = { Authorization: `Bearer ${token}` };
+  
+    axios
+      .all([
+        axios.get(`http://localhost:8080/admin/events/convidado/${userId}`, { headers }),
+        axios.get(`http://localhost:8080/admin/events/respostas/${userId}`, { headers })
+        .then(res => res)
+        .catch((err) => {
+          // Se a rota de respostas não encontrar nada, retorna array vazio
+          if (err.response?.status === 404) {
+            console.warn("Nenhuma resposta encontrada — retornando lista vazia.");
+            return { data: [] };
+          }
+          // Se for outro tipo de erro, relança para o catch principal
+          throw err;
+        }),
+      ])
+      .then(([resEventos, resFormEvento]) => {
+        const eventos = resEventos.data;
+        setEventos(eventos);
+  
+        const counts = {
+          total: eventos.length,
+          aguardando: 0
+        };
+
+        eventos.forEach((evento: any) => {
+          const status = evento.status?.toLowerCase()
+
+          if (status !== "confirmado" && status !== "recusado") counts.aguardando++
+        });
+
+        setStatusCounts(counts)
+  
+        const respForm = resFormEvento.data
+          setEventoResp(respForm)
+
+          const countsFormEvento = {
+            respondido: respForm.length
+          };
+
+          setEventoRespCount(countsFormEvento)
+      })
+      .catch((err) => console.error("Erro ao buscar eventos do usuário:", err));
+      
+  }, [userId, token]);
+  
 
   // Renderização Principal
   return (
@@ -94,54 +155,61 @@ const irEventosColaborador = () =>{
       <Header user={mockHeader.user} placeholderAvatar={mockHeader.user.avatarUrl} />
       <Navbar />
 
+      <div>
+        <h1 className='text-azul-principal ml-[5%] mt-10 text-[30px] font-bold drop-shadow-[5px_5px_3px_rgba(0,0,0,0.3)]'>Verifique as informações dos seus eventos</h1>
+      </div>
       <div className="p-8  pb-2 grid grid-cols-3 gap-x-5 ml-10">
+
         <HighlightCard
-          title="Confirmar presença/preencher"
-          value={mockData3.eventos}
+          title="Ver Eventos"
+          value={statusCounts.total}
           subtitle={mockEventos.subtitle}
           variant="primary"
           icon={FaCalendar}
+          onClick={irEventos}
+        />
+
+        <HighlightCard
+          title="Confirmar presença/preencher"
+          value={statusCounts.aguardando}
+          subtitle={mockEventos.subtitle}
+          variant="primary"
+          icon={FaRegEdit}
           onClick={irEventosColaborador}
         />
 
         <HighlightCard
           title=" Ver formulário de aproveitamento"
-          value={mockData2.checkRespondido}
+          value={eventoRespCount.respondido}
           subtitle={mockCheckPreenchidos.subtitle}
           variant="primary"
           icon={FaEye}
           onClick={irRespostaEventos}
         />
-
-        <HighlightCard
-          title="Confirmar presença/preencher"
-          value={mockData3.eventos}
-          subtitle={mockEventos.subtitle}
-          variant="primary"
-          icon={FaCalendar}
-          onClick={irEventosColaborador}
-        />
       </div>
-      <div className="p-8 pt-2 grid grid-cols-2 gap-x-5 ml-10 mt-0">
+      <div>
+        <h1 className='text-azul-principal ml-[5%] mt-10 text-[30px] font-bold drop-shadow-[5px_5px_3px_rgba(0,0,0,0.3)]'>Verifique as informações dos seus formulários e checklists</h1>
+      </div>
+      <div className="p-8 pt-2 grid grid-cols-3 gap-x-5 ml-10 mt-0">
         <HighlightCard
           title="Preencher Checklists"
           value={mockData.checkResponder}
           subtitle={mockCheckPreencher.subtitle}
           variant="primary"
           icon={FaRegEdit}
-          onClick={irEventosCOlaborador}
+          onClick={irCheckColaborador}
         />
 
         <HighlightCard
-          title="Ver Eventos"
+          title="Ver checklists preenchidos"
           value={mockData3.eventos}
           subtitle={mockEventos.subtitle}
           variant="primary"
-          icon={FaCalendar}
-          onClick={irEventos}
+          icon={FaEye}
+          onClick={irCheckColaborador}
         />
       </div >
-      
+
     </>
   );
 };
