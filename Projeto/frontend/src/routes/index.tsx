3b040bcf42abaac3ba42481bbe958d9c8e354668
kakashinho <joaovitor.s.c.siqueira@gmail.com>
2025-10-24 09:07:18 -0300

    <feat>(US05): adiciona o contexto para compartilhar informações com os outros componentes do React.

--- Projeto/frontend/src/routes/index.tsx
+++ Projeto/frontend/src/routes/index.tsx
@@ -1,12 +1,15 @@
 import { BrowserRouter, Routes } from "react-router-dom";
 import RotasADM from "./administrativo";
+import { AuthProvider } from "../contexts/AuthContext"; 
 
 export default function RotasGerais(){
     return(
         <BrowserRouter>
-            <Routes>
-                {RotasADM()}
-            </Routes>
+            <AuthProvider>
+                <Routes>
+                    {RotasADM()}
+                </Routes>
+            </AuthProvider>
         </BrowserRouter>
     )
 }
