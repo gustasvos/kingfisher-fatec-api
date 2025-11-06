7655a80c73b04a2bacfea3e69cae045b47f73637
Gustavo Rosa <gustavorib.rosa@gmail.com>
2025-11-06 12:15:59 -0300

    <feat>(US08): criação das tabelas registro_cliente, agendamento_cliente, categoria_cliente

--- Projeto/backend/src/modules/models/registroCliente.ts
+++ Projeto/backend/src/modules/models/registroCliente.ts
@@ -1,5 +1,6 @@
 import { Entity, PrimaryGeneratedColumn, Column, ManyToOne, JoinColumn } from 'typeorm'
 import { Cliente } from './cliente'
+import { ClienteCategoria } from './clienteCategoria'
 
 @Entity('registro_cliente')
 export class RegistroCliente {
@@ -9,42 +10,20 @@ export class RegistroCliente {
     @Column()
     cliente_id: number
 
+    @Column()
+    categoria_id: number
+
     @Column({ type: 'date' })
     data_registro: Date
 
     @Column({ type: 'varchar', length: 500 })
     observacao: string
 
-    // FK 
     @ManyToOne(() => Cliente, (cliente) => cliente.id, { onDelete: 'CASCADE' })
     @JoinColumn({ name: 'cliente_id' })
     cliente: Cliente
+
+    @ManyToOne(() => ClienteCategoria)
+    @JoinColumn({ name: 'categoria_id' })
+    categoria: ClienteCategoria    
 }
-
-
-/*
-Buscar a categoria com left join
-
-const registros = await dataSource
-    .getRepository(RegistroCliente)
-    .createQueryBuilder('registro')
-    .leftJoinAndSelect('registro.cliente', 'cliente')
-    .select([
-        'registro.id',
-        'registro.data_registro',
-        'registro.observacao',
-        'cliente.id',
-        'cliente.Categoria', // pega só os campos desejados do cliente
-    ])
-    .getMany()
-
-Ou
-
-const registro = await registroClienteRepository.findOne({
-  where: { id: 1 },
-  relations: ['cliente'],
-})
-
-console.log(registro.cliente.Categoria)
-
-*/
