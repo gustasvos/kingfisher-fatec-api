c1eb1421002775a2d4a90c13aceae8256d0baa59
Lucas Inacio de Carvalho <lucasinacio8000@gmail.com>
2025-11-22 17:34:16 -0300

    <fix>(US-14):Ajustando duplicação de funções

--- Projeto/backend/src/modules/controllers/cotacaoController.ts
+++ Projeto/backend/src/modules/controllers/cotacaoController.ts
@@ -4,7 +4,6 @@ import { Cotacao } from "../models/cotacao"
 import { Cliente } from "../models/cliente"
 import { DeepPartial } from "typeorm";
 import nodemailer from "nodemailer"
-import nodemailer from "nodemailer"
 import fs from "fs";
 import path from "path";
 import dotenv from 'dotenv'
@@ -226,86 +225,6 @@ export const enviarEmailCotacao = async (req: Request, res: Response) => {
     }
 }
 
-// POST /cotacao/enviar-email
-export const enviarEmailCotacao = async (req: Request, res: Response) => {
-    try {
-        const { cotacaoId, template } = req.body
-
-        if (!cotacaoId || !template) {
-            return res.status(400).json({ message: "cotacaoId ou template vazio." })
-        }
-
-        const cotacaoRepository = AppDataSource.getRepository(Cotacao)
-
-        const cotacao = await cotacaoRepository.findOne({
-            where: { id: cotacaoId },
-            relations: ["cliente"]
-        });
-
-        if (!cotacao) {
-            return res.status(404).json({ message: "Cotação não encontrada." })
-        }
-        let detalhesFrete: any = {};
-        try {
-            detalhesFrete = JSON.parse(cotacao.detalhes_frete);
-        } catch (err) {
-            detalhesFrete = {};
-        }
-
-        // Monta uma string legível para o usuário
-        const detalhesFreteLegivel = `Mercadoria: ${detalhesFrete.mercadoria || "-"}<br/>
-        Local de Coleta: ${detalhesFrete.localColeta || "-"}<br/>
-        Local de Entrega: ${detalhesFrete.localEntrega || "-"}<br/>
-        Peso Estimado: ${detalhesFrete.pesoEstimado || "-"}<br/>
-        Tipo de Veículo: ${detalhesFrete.tipoVeiculo || "-"}
-        `;
-
-        // substitui variáveis no template
-        let htmlFinal = template
-            .replace(/{{valor_total}}/g, cotacao.valor_total ? Number(cotacao.valor_total).toFixed(2) : "0.00")
-            .replace(/{{nomeFantasia}}/g, cotacao.cliente.nomeFantasia)
-            .replace(/{{CNPJ}}/g, cotacao.cliente.CNPJ || "")
-            .replace(/{{contatoResponsavel}}/g, cotacao.cliente.contatoResponsavel || "")
-            .replace(/{{emailResponsavel}}/g, cotacao.cliente.emailResponsavel || "")
-            .replace(/{{data_criacao}}/g, cotacao.data_criacao.toISOString().slice(0, 10))
-            .replace(/{{data_validade}}/g, cotacao.data_validade ? new Date(cotacao.data_validade).toISOString().slice(0, 10) : "")
-            .replace(/{{detalhes_frete}}/g, detalhesFreteLegivel || "")
-
-        // configura o transport
-        const transporter = nodemailer.createTransport({
-            service: "gmail",
-            auth: {
-                user: process.env.GMAIL_USER,
-                pass: process.env.GMAIL_APP_PASSWORD
-            }
-        })
-
-        await transporter.sendMail({
-            from: `"Newe Logística" <${process.env.GMAIL_USER}>`,
-            to: process.env.EMAIL_RECEIVER,
-            subject: `Nova Cotação — Cliente ${cotacao.cliente.nomeFantasia}`,
-            html: htmlFinal,
-            attachments: [
-                {   //Projeto\backend\uploads\cotacoes\2025\11\cotacao_1.pdf
-                    filename: `Cotacao-${cotacao.cliente.nomeFantasia}.pdf`, // nome do arquivo que será enviado
-                    path: path.join(cotacoesDir, '../../', cotacao.caminho_arquivo_pdf!) // caminho do PDF no servidor
-                }
-            ]
-        }).catch(e => {
-            console.error(e.response || e); // mostra erro real do Gmail
-            throw e;
-        });
-
-        return res.status(200).json({ message: "E-mail enviado com sucesso!" })
-
-    } catch (error) {
-        console.error("Erro ao enviar e-mail:", error)
-        // console.log("USER:", process.env.GMAIL_USER);
-        // console.log("PASS:", process.env.GMAIL_APP_PASSWORD);
-        return res.status(500).json({ message: "Erro ao enviar e-mail." })
-
-    }
-}
 
 // GET /cotacao/ultima/:clienteId
 export const getUltimaCotacaoByCliente = async (req: Request, res: Response) => {
